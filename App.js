759ebeb4f06a8941836127ff0528f167225b343c
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-09-23 11:27:45 +0000

    f

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Meunome from './componentes/primeiro-componente';
 
 export default function App() {
   return (
---
+++ componentes/primeiroComponente.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+export default function Meunome(){
+    return(
+        <>
+        <h1>Eduarda</h1>
+        </>
+    )
+    
+}
