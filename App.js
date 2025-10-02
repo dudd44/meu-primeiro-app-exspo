8675143d3653a656b9027712c7e47f0f9ce45740
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-10-02 14:12:51 +0000

    33

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 
 export default function App() {
+
+  const [fonteCarregada] = useFonts({
+    "Montserrat_400Regular": Montserrat_400Regular,
+     "Montserrat_700Bold":  Montserrat_700Bold,
+  });
+
+  if(!fonteCarregada){
+    return(<>
+         </>
+         )
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
