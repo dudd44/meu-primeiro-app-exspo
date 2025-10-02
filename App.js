5bb3e7262abeaa5effea0c68be34badbbc54ccb2
dudd44 <luciano.eduarda@escola.pr.gov.br>
2025-10-02 13:07:17 +0000

    a

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Meunome from './componentes/primeiro-componente';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Dudão</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <StatusBar />
+
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7700ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
